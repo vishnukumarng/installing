dbee272252c2ca9946e0fb6595bcc8ad818e046b
Vishnukumar <156579678+vishnukumarng@users.noreply.github.com>
2024-07-16 05:48:28 +0530

    message : there is no need for n variable

--- zoho.java
+++ zoho.java
@@ -1,12 +1,11 @@
 public class zoho {
 	public static void main(String args[]) {
 		int c=1;
-        int n=5;
-		for(int i=1;i<=n;i++)
+		for(int i=1;i<=5;i++)
 		{
 			int a=i+1,count=0,b=c;
 			System.out.print(c+" ");
-			for(int j=1;j<=n-1;j++)
+			for(int j=1;j<=4;j++)
 			{
 				if(i==5)
 				{
