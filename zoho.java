d150208e8575636d153d0243988643962283dc99
vishnukumarng <ngvk172005@gmail.com>
2024-07-16 05:45:31 +0530

    introducing n variable

--- zoho.java
+++ zoho.java
@@ -1,11 +1,12 @@
 public class zoho {
 	public static void main(String args[]) {
 		int c=1;
-		for(int i=1;i<=5;i++)
+        int n=5;
+		for(int i=1;i<=n;i++)
 		{
 			int a=i+1,count=0,b=c;
 			System.out.print(c+" ");
-			for(int j=1;j<=4;j++)
+			for(int j=1;j<=n-1;j++)
 			{
 				if(i==5)
 				{
